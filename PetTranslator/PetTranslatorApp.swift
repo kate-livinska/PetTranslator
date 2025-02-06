843cd3970851c2f6b12a9467041b3fd88491febb
Kateryna Livinska <kateryna.livinska@gmail.com>
2025-02-06 16:31:38 +0100

    Initial Commit

---
+++ PetTranslator/PetTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetTranslatorApp.swift
+//  PetTranslator
+//
+//  Created by Kateryna Livinska on 06/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PetTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
